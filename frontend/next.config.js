022aadb7b666d6f39b6d7b86b5ef0f85ef6a69eb
Nick-Maximillien <nicholasmuthoki@gmail.com>
2025-05-21 23:59:56 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental: {
-      appDir: true,
-      typedRoutes: true,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost", 
-          port: "8000",
-          pathname: "/media/**", // Adjusted for your media paths
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+    typedRoutes: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "https",
+        hostname: "online-store-production-b3b2.up.railway.app",
+        pathname: "/media/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
